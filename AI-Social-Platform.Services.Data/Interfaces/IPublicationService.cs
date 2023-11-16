7d5dfe8b6c09aeaba0fd228b150b5a88af60e4bd
Bagie <Bagriyan.dilyanov@abv.bg>
2023-11-16 19:25:41 +0200

    Adding publicationService interface

---
+++ AI-Social-Platform.Services.Data/Interfaces/IPublicationService.cs
@@ -0,0 +1,23 @@
+﻿using AI_Social_Platform.Services.Data.Models.PublicationDtos;
+
+namespace AI_Social_Platform.Services.Data.Interfaces;
+
+public interface IPublicationService
+{
+    public Task<IEnumerable<PublicationDto>> GetPublicationsAsync();
+    public Task<PublicationDto> GetPublicationAsync(Guid id);
+    public Task CreatePublicationAsync(PublicationFormDto dto);
+
+    public Task UpdatePublicationAsync(PublicationFormDto dto, Guid id);
+
+    public Task DeletePublicationAsync(Guid id);
+
+    public Task CreateCommentAsync(CommentFormDto dto);
+
+    public Task UpdateCommentAsync(CommentFormDto dto, Guid id);
+
+    public Task DeleteCommentAsync(Guid id);
+
+  
+}
+
