aeab93b2c57fd70a05e1580eca46a7dc2ed498cf
Bagriyan Borisov <108102199+BagriyanBorisov@users.noreply.github.com>
2023-11-18 16:15:39 +0200

    Fix scaffolding of user identity

--- AI-Social-Platform.Server/Program.cs
+++ AI-Social-Platform.Server/Program.cs
@@ -1,6 +1,10 @@
+using AI_Social_Platform.Common;
 using AI_Social_Platform.Data;
+using AI_Social_Platform.Data.Models;
 using AI_Social_Platform.Services.Data;
 using AI_Social_Platform.Services.Data.Interfaces;
+using AI_Social_Platform.Services.Data.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -12,8 +16,12 @@ builder.Services.AddDbContext<ASPDbContext>(options =>
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ASPDbContext>();
+
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddScoped<IUserService, UserService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
