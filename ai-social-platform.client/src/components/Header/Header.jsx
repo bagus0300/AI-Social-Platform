f6d4075887523ae1fe864bc2cff5a3594f1c3dff
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-19 18:48:08 +0200

    Update header component. Add logo search form and buttons

--- ai-social-platform.client/src/components/Header/Header.jsx
+++ ai-social-platform.client/src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
+import { Link } from 'react-router-dom';
+
 import styles from './Header.module.css';
 
 export default function Header() {
     return (
         <header className={styles['app-header']}>
-            <p>Header</p>
+            <Link to={'/'}>
+                <h2>AI-Social-Platform</h2>
+            </Link>
+            <form className={styles['search-form']}>
+                <input type="search" placeholder="Search..." />
+                <button>Search</button>
+            </form>
+            <section className={styles['profile']}>
+                <Link to={'/login'}>Login</Link>
+                <Link to={'/register'}>Register</Link>
+            </section>
         </header>
     );
 }
